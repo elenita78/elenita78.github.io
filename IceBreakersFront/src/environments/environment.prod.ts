a84979a98e7ff3560e9077d86456981a18f580fa
elenita78 <elenanavarrom@gmail.com>
2020-06-04 18:40:50 +0200

    modify environm prod

--- IceBreakersFront/src/environments/environment.prod.ts
+++ IceBreakersFront/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB-8Zk20RgoE2xvn5kdsnkzt-nQ0K1X09E",
+    authDomain: "icebeakers-8e7a8.firebaseapp.com",
+    databaseURL: "https://icebeakers-8e7a8.firebaseio.com",
+    projectId: "icebeakers-8e7a8",
+    storageBucket: "icebeakers-8e7a8.appspot.com",
+    messagingSenderId: "438906592704",
+    appId: "1:438906592704:web:4c7936caf7e1c46f13bc25"
+  }
 };
+
