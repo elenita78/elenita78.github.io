4a391dfab24d400c884cb1bdf3a0cd8b07829c94
elenita78 <elenanavarrom@gmail.com>
2020-06-09 18:12:18 +0200

    routing page

--- IceBreakersFront/src/app/app-routing.module.ts
+++ IceBreakersFront/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameCoverComponent } from './components/game-cover/game-cover.component';
+import { PlayerComponent } from './components/player/player.component';
+import { GameFacilitationComponent } from './components/game-facilitation/game-facilitation.component';
 
 
 const routes: Routes = [
-  {path:'', component:GameCoverComponent}
+  {path:'', component:GameCoverComponent},
+  {path:'player', component:PlayerComponent},
+  {path:'facilitation', component:GameFacilitationComponent}
 ];
 
 @NgModule({
